0176ec1f0971714de093cb1eec6c924d6ee649e5
diogenes1027 <a01653251@itesm.mx>
2022-08-22 18:26:14 -0500

    Doors script

---
+++ Ejercicios/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class DoorScript : MonoBehaviour
+{
+    private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && _animator.GetBool("Open") == false) {
+            _animator.SetBool("Open", true);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.A) && _animator.GetBool("Open") == true)
+        {
+            _animator.SetBool("Open", false);
+        }
+    }
+}
