a3e5a5d3ded5b378b039ed7f07075bdcc0acddae
diogenes1027 <a01653251@itesm.mx>
2022-08-23 19:57:53 -0500

    class 23 agust

---
+++ Ejercicios/Assets/Scripts/Granade.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Granade : MonoBehaviour
+{
+    public float wait;
+    bool exploted = false;
+    public float radius = 6f;
+    public float force = 500f;
+    public GameObject explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wait = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wait -= Time.deltaTime;
+        if (wait <= 0f && !exploted) {
+            exploted = true;
+            explote();
+        }
+    }
+
+    void explote() {
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        Collider[] col = Physics.OverlapSphere(transform.position,radius);
+
+        foreach (Collider cercano in col) {
+            Rigidbody rb = cercano.GetComponent<Rigidbody>();
+
+            if (rb != null) {
+                rb.AddExplosionForce(force,transform.position,radius);
+            }
+        }
+
+        Destroy(gameObject);
+        Destroy(GameObject.Find("BigExplosionEffect(Clone)"), 3f);
+       
+    }
+}
---
+++ Ejercicios/Assets/Scripts/Ship.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 Vec = transform.localPosition;
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            Vec.x += Input.GetAxis("Horizontal") * Time.deltaTime * 20;
+            _animator.SetBool("Right", true);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            Vec.x += Input.GetAxis("Horizontal") * Time.deltaTime * 20;
+            _animator.SetBool("Left", true);
+        }
+        else {
+            _animator.SetBool("Right", false);
+            _animator.SetBool("Left", false);
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            _animator.SetBool("Front", true);
+            
+            Vec.z += Input.GetAxis("Vertical") * Time.deltaTime * 20;
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            Vec.z += Input.GetAxis("Vertical") * Time.deltaTime * 20;
+            _animator.SetBool("Back", true);
+        }
+        else {
+            _animator.SetBool("Front", false);
+            _animator.SetBool("Back", false);
+        }
+        transform.localPosition = Vec;
+    }
+}
