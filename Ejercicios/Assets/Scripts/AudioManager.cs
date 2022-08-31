dc81407e2dcea88d86f6d6d9a352790cdd9c87bc
diogenes1027 <a01653251@itesm.mx>
2022-08-30 21:04:08 -0500

    30 august class

---
+++ Ejercicios/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] sonidos;
+    private AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void AudioSelect(int clip, float volume) {
+
+        audio.Stop();
+        audio.PlayOneShot(sonidos[clip],volume);
+    }
+
+}
---
+++ Ejercicios/Assets/Scripts/Personaje.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    private AudioManager audioManager;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S)) {
+
+            audioManager.AudioSelect(0, 0.5f);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+            audioManager.AudioSelect(1, 0.5f);
+        }
+    }
+}
