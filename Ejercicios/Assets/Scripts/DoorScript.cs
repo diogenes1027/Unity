2248369dcdbfd8a333bcebee56d37f81e2b801b8
diogenes1027 <a01653251@itesm.mx>
2022-09-02 17:29:44 -0500

    door sound

--- Ejercicios/Assets/Scripts/DoorScript.cs
+++ Ejercicios/Assets/Scripts/DoorScript.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class DoorScript : MonoBehaviour
 {
     private Animator _animator;
+    private AudioManager audioManager;
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
+        audioManager = FindObjectOfType<AudioManager>();
     }
 
     // Update is called once per frame
@@ -17,11 +19,13 @@ public class DoorScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.A) && _animator.GetBool("Open") == false) {
             _animator.SetBool("Open", true);
+            audioManager.AudioSelect(0, 0.5f);
         }
 
         else if (Input.GetKeyDown(KeyCode.A) && _animator.GetBool("Open") == true)
         {
             _animator.SetBool("Open", false);
+            audioManager.AudioSelect(1, 0.5f);
         }
     }
 }
