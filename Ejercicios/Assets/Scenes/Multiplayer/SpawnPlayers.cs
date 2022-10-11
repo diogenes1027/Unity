2df8adb8e0bf017c19c5d804d961907d65fc9073
diogenes1027 <a01653251@itesm.mx>
2022-10-10 22:14:34 -0500

    multiplayer

---
+++ Ejercicios/Assets/Scenes/Multiplayer/SpawnPlayers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SpawnPlayers : MonoBehaviour
+{
+    public GameObject playerPrefab;
+
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 randomPosition = new Vector3(Random.Range(minX,maxX),2.06f,Random.Range(minY,maxY));
+        PhotonNetwork.Instantiate(playerPrefab.name,randomPosition,Quaternion.identity) ;
+    }
+}
--- Ejercicios/Assets/Scripts/Car.cs
+++ Ejercicios/Assets/Scripts/Car.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class Car : MonoBehaviour
 {
@@ -13,7 +14,7 @@ public class Car : MonoBehaviour
 
     public float acceleration = 500f;
     public float breakingforce = 300f;
-    float turn = 25f;
+    float turn = 15f;
 
     float currentacceleration = 0f;
     float currentbreakingforce = 0f;
@@ -21,51 +22,58 @@ public class Car : MonoBehaviour
 
     AudioSource audio;
 
+    PhotonView view;
+
 
     // Start is called before the first frame update
     void Start()
     {
         audio = GetComponent<AudioSource>();
+        view = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        currentacceleration = acceleration * Input.GetAxis("Vertical");
+        if (view.IsMine) {
+            currentacceleration = acceleration * Input.GetAxis("Vertical");
 
 
-        if (Input.GetKey(KeyCode.Space))
-        {
-            currentbreakingforce = breakingforce;
-            _animator.SetBool("ON", true);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                currentbreakingforce = breakingforce;
+                _animator.SetBool("ON", true);
+            }
+
+            else
+            {
+                currentbreakingforce = 0f;
+                _animator.SetBool("ON", false);
+            }
+
+
+            fr.motorTorque = currentacceleration * 100f;
+            fl.motorTorque = currentacceleration;
+            bl.motorTorque = currentacceleration;
+            bl.motorTorque = currentacceleration;
+
+            fr.brakeTorque = currentbreakingforce;
+            fl.brakeTorque = currentbreakingforce;
+            br.brakeTorque = currentbreakingforce;
+            bl.brakeTorque = currentbreakingforce;
+
+            currentturn = turn * Input.GetAxis("Horizontal");
+            fr.steerAngle = currentturn;
+            fl.steerAngle = currentturn;
+            br.steerAngle = -1f * currentturn;
+            bl.steerAngle = -1f * currentturn;
+
+
+
+            if (Input.GetKey(KeyCode.X)) gameObject.transform.localRotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, 0);
+
+            if (Input.GetKey(KeyCode.C)) audio.PlayOneShot(honk, 0.5f);
         }
-
-        else {
-            currentbreakingforce = 0f;
-            _animator.SetBool("ON", false);
-        }
-        
-
-        fr.motorTorque = currentacceleration*100f;
-        fl.motorTorque = currentacceleration;
-        bl.motorTorque = currentacceleration;
-        bl.motorTorque = currentacceleration;
-
-        fr.brakeTorque = currentbreakingforce;
-        fl.brakeTorque = currentbreakingforce;
-        br.brakeTorque = currentbreakingforce;
-        bl.brakeTorque = currentbreakingforce;
-
-        currentturn = turn * Input.GetAxis("Horizontal");
-        fr.steerAngle = currentturn;
-        fl.steerAngle = currentturn;
-        br.steerAngle = -1f*currentturn;
-        bl.steerAngle = -1f*currentturn;
-
-
-
-        if (Input.GetKey(KeyCode.X)) gameObject.transform.localRotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, 0);
-
-        if (Input.GetKey(KeyCode.C)) audio.PlayOneShot(honk, 0.5f);
     }
+        
 }
