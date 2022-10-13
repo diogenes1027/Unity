b94e44fe34c22b7464b23e47697bea506aad7ae3
Diógenes Grajales Corona <a01653251@itesm.mx>
2022-10-13 11:34:13 -0500

    13 10

--- Ejercicios/Assets/Scripts/ArmRobot.cs
+++ Ejercicios/Assets/Scripts/ArmRobot.cs
@@ -1,18 +1,126 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static RobotBody;
 
 public class ArmRobot : MonoBehaviour
 {
+    string side;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public void Init(string _side, ref List<GameObject> go_parts, ref List<Matrix4x4> m_locations, ref List<Matrix4x4> m_scales) 
+    {
+        if (_side == "LEFT") 
+        {
+            side = "LEFT";
+            //LSHOULDER
+            INIT_PART(ref go_parts, ref m_locations, ref m_scales, PrimitiveType.Cube, (int)PARTS.LSHOULDER, Color.red, "LSHOULDER", new Vector3(0.4f, 0.4f, 0.4f), new Vector3(0f, 0.17f, 0.78f));
+            //LARM
+            INIT_PART(ref go_parts, ref m_locations, ref m_scales, PrimitiveType.Cube, (int)PARTS.LARM, Color.white, "LARM", new Vector3(0.3f, 0.3f, 0.4f), new Vector3(0f, 0f, 0.4f));
+            //LFOREARM
+            INIT_PART(ref go_parts, ref m_locations, ref m_scales, PrimitiveType.Cube, (int)PARTS.LFOREARM, Color.red, "LFOREARM", new Vector3(0.4f, 0.4f, 0.7f), new Vector3(0f, 0f, 0.55f));
+            //LSHOULDER
+            INIT_PART(ref go_parts, ref m_locations, ref m_scales, PrimitiveType.Cube, (int)PARTS.LHAND, Color.blue, "LHAND", new Vector3(0.3f, 0.3f, 0.3f), new Vector3(0f, 0f, 0.5f));
+
+        }
+        else if (_side == "RIGHT")
+        {
+            side = "RIGHT";
+            //RSHOULDER
+            INIT_PART(ref go_parts, ref m_locations, ref m_scales, PrimitiveType.Cube, (int)PARTS.RSHOULDER, Color.red, "RSHOULDER", new Vector3(0.4f, 0.4f, 0.4f), new Vector3(0f, 0.17f, -0.78f));
+            //RARM
+            INIT_PART(ref go_parts, ref m_locations, ref m_scales, PrimitiveType.Cube, (int)PARTS.RARM, Color.white, "RARM", new Vector3(0.3f, 0.3f, 0.4f), new Vector3(0f, 0f, -0.4f));
+            //RFOREARM
+            INIT_PART(ref go_parts, ref m_locations, ref m_scales, PrimitiveType.Cube, (int)PARTS.RFOREARM, Color.red, "RFOREARM", new Vector3(0.4f, 0.4f, 0.7f), new Vector3(0f, 0f, -0.55f));
+            //RSHOULDER
+            INIT_PART(ref go_parts, ref m_locations, ref m_scales, PrimitiveType.Cube, (int)PARTS.RHAND, Color.blue, "RHAND", new Vector3(0.3f, 0.3f, 0.3f), new Vector3(0f, 0f, -0.5f));
+
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Draw(ref Matrix4x4 chestMatrix, ref List<GameObject> go_parts, List<Matrix4x4> m_locations, List<Matrix4x4> m_scales, BACK_FORTH rX, Vector3[] v3_originals)
+    {
+        Matrix4x4 accumT = Matrix4x4.identity;
+        if (side == "LEFT") 
+        {
+            for (int i = (int)PARTS.LSHOULDER; i <= (int)PARTS.LHAND; i++) 
+            {
+                Matrix4x4 m = accumT * m_locations[i] * m_scales[i];
+                if (i == (int)PARTS.LSHOULDER)
+                {
+                    accumT = chestMatrix;
+                    m = accumT * m_locations[i] * m_scales[i];
+                    Matrix4x4 r = Transformations.RotateX(90);
+                    accumT *= m_locations[i] * r;
+                }
+                else if (i == (int)PARTS.LARM)
+                {
+                    Matrix4x4 t1 = Transformations.Translate(0f, 0f, 0.2f);
+                    Matrix4x4 t2 = Transformations.Translate(0f, 0f, 0.2f);
+                    Matrix4x4 r = Transformations.RotateY(rX.val);
+                    m = accumT * t1 * r * t2 * m_scales[i];
+                    accumT *= t1 * r * t2; 
+                }
+                else if (i == (int)PARTS.LFOREARM)
+                {
+                    Matrix4x4 t1 = Transformations.Translate(0f, 0f, 0.2f);
+                    Matrix4x4 t2 = Transformations.Translate(0f, 0f, 0.35f);
+                    Matrix4x4 r = Transformations.RotateY(-20);
+                    m = accumT * t1 * r * t2 * m_scales[i];
+                    accumT *= t1 * r * t2;
+                }
+                else accumT *= m_locations[i];
+
+                go_parts[i].GetComponent<MeshFilter>().mesh.vertices = Transformations.Transform(m, v3_originals);
+            }
+        }
+        else if (side == "RIGHT")
+        {
+            for (int i = (int)PARTS.RSHOULDER; i <= (int)PARTS.RHAND; i++)
+            {
+                Matrix4x4 m = accumT * m_locations[i] * m_scales[i];
+
+                if (i == (int)PARTS.RSHOULDER)
+                {
+                    accumT = chestMatrix;
+                    m = accumT * m_locations[i] * m_scales[i];
+                    Matrix4x4 r = Transformations.RotateX(-90);
+                    accumT *= m_locations[i] * r;
+                }
+                else if (i == (int)PARTS.RARM)
+                {
+                    Matrix4x4 t1 = Transformations.Translate(0f, 0f, -0.2f);
+                    Matrix4x4 t2 = Transformations.Translate(0f, 0f, -0.2f);
+                    Matrix4x4 r = Transformations.RotateY(rX.val);
+                    m = accumT * t1 * r * t2 * m_scales[i];
+                    accumT *= t1 * r * t2; 
+                }
+                else if (i == (int)PARTS.RFOREARM)
+                {
+                    Matrix4x4 t1 = Transformations.Translate(0f, 0f, -0.2f);
+                    Matrix4x4 t2 = Transformations.Translate(0f, 0f, -0.35f);
+                    Matrix4x4 r = Transformations.RotateY(20);
+                    m = accumT * t1 * r * t2 * m_scales[i];
+                    accumT *= t1 * r * t2;
+                }
+                else {
+                   
+                    accumT *= m_locations[i];
+                }
+                
+
+                go_parts[i].GetComponent<MeshFilter>().mesh.vertices = Transformations.Transform(m, v3_originals);
+            }
+        }
     }
 }
--- Ejercicios/Assets/Scripts/LegRobot.cs
+++ Ejercicios/Assets/Scripts/LegRobot.cs
@@ -1,18 +1,142 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static RobotBody;
 
 public class LegRobot : MonoBehaviour
 {
+    string side;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    public void Init(string _side, ref List<GameObject> go_parts, ref List<Matrix4x4> m_locations, ref List<Matrix4x4> m_scales)
+    {
+        if (_side == "LEFT")
+        {
+            side = "LEFT";
+            //LTHIGH
+            INIT_PART(ref go_parts, ref m_locations, ref m_scales, PrimitiveType.Cube, (int)PARTS.LTHIGH, Color.white, "LTHIGH", new Vector3(0.4f, 0.8f, 0.4f), new Vector3(0f, -0.575f, 0.32f));
+            //LLEG
+            INIT_PART(ref go_parts, ref m_locations, ref m_scales, PrimitiveType.Cube, (int)PARTS.LLEG, Color.blue, "LLEG", new Vector3(0.5f, 1f, 0.5f), new Vector3(0f, -0.9f, 0.01f));
+            //LFOOT
+            INIT_PART(ref go_parts, ref m_locations, ref m_scales, PrimitiveType.Cube, (int)PARTS.LFOOT, Color.blue, "LFOOT", new Vector3(0.7f, 0.3f, 0.5f), new Vector3(-0.103f, -0.625f, 0));
+
+        }
+        else if (_side == "RIGHT")
+        {
+            side = "RIGHT";
+            //RTHIGH
+            INIT_PART(ref go_parts, ref m_locations, ref m_scales, PrimitiveType.Cube, (int)PARTS.RTHIGH, Color.white, "RTHIGH", new Vector3(0.4f, 0.8f, 0.4f), new Vector3(0f, -0.575f, -0.32f));
+            //RLEG
+            INIT_PART(ref go_parts, ref m_locations, ref m_scales, PrimitiveType.Cube, (int)PARTS.RLEG, Color.blue, "RLEG", new Vector3(0.5f, 1f, 0.5f), new Vector3(0f, -0.9f, -0.01f));
+            //RFOOT
+            INIT_PART(ref go_parts, ref m_locations, ref m_scales, PrimitiveType.Cube, (int)PARTS.RFOOT, Color.blue, "RFOOT", new Vector3(0.7f, 0.3f, 0.5f), new Vector3(-0.103f, -0.625f, 0));
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void Draw(ref Matrix4x4 heapMatrix, ref List<GameObject> go_parts, List<Matrix4x4> m_locations, List<Matrix4x4> m_scales, BACK_FORTH rX, Vector3[] v3_originals, BACK_FORTH rk)
+    {
+        Matrix4x4 accumT = Matrix4x4.identity;
+        float movementLegL;
+        float movementLegR;
+
+
+        if (side == "LEFT")
+        {
+            for (int i = (int)PARTS.LTHIGH; i <= (int)PARTS.LFOOT; i++)
+            {
+                Matrix4x4 m = accumT * m_locations[i] * m_scales[i];
+                if (i == (int)PARTS.LTHIGH)
+                {
+                    accumT = heapMatrix;
+                    Matrix4x4 t1 = Transformations.Translate(0f, -0.175f, 0.32f);
+                    Matrix4x4 t2 = Transformations.Translate(0f, -0.4f, 0.0f);
+                    Matrix4x4 r = Transformations.RotateZ(rX.val * 3f);
+                    m = accumT * t1 * r * t2 * m_scales[i];
+                    accumT *= t1 * r * t2;
+
+                }
+                else if (i == (int)PARTS.LLEG)
+                {
+                    Matrix4x4 t1 = Transformations.Translate(0f, -0.4f, 0f);
+                    Matrix4x4 t2 = Transformations.Translate(0f, -0.5f, 0f);
+                    movementLegL = Mathf.Max(rX.val * 3.5f, 0);
+                    Matrix4x4 r = Transformations.RotateZ(movementLegL);
+                    m = accumT * t1 * r * t2 * m_scales[i];
+                    accumT *= t1 * r * t2;
+                }
+                else 
+                {
+                    Matrix4x4 t1 = Transformations.Translate(0f, -0.5f, 0f);
+                    Matrix4x4 t2 = Transformations.Translate(-0.103f, -0.15f, 0f);
+                    movementLegL = Mathf.Max(-rX.val * 4f, 0);
+                    Matrix4x4 r = Transformations.RotateZ(rX.val * 2f);
+                    m = accumT * t1 * r * t2 * m_scales[i];
+                    accumT *= t1 * r * t2;
+                } 
+
+                go_parts[i].GetComponent<MeshFilter>().mesh.vertices = Transformations.Transform(m, v3_originals);
+            }
+        }
+        else if (side == "RIGHT")
+        {
+            float currR = 0;
+            for (int i = (int)PARTS.RTHIGH; i <= (int)PARTS.RFOOT; i++)
+            {
+                
+                Matrix4x4 m = accumT * m_locations[i] * m_scales[i];
+                if (i == (int)PARTS.RTHIGH)
+                {
+                    accumT = heapMatrix;
+                    Matrix4x4 t1 = Transformations.Translate(0f, -0.175f, -0.32f);
+                    Matrix4x4 t2 = Transformations.Translate(0f, -0.4f, 0.0f);
+                    //Matrix4x4 r = Transformations.RotateZ(-rX.val * 3f);
+                    currR = rX.val;
+                    movementLegL = Mathf.Min(-rX.val * 5f, 20);
+                    Matrix4x4 r = Transformations.RotateZ(-rX.val * 5f);
+                    m = accumT * t1 * r * t2 * m_scales[i];
+                    accumT *= t1 * r * t2;
+
+                }
+                else if (i == (int)PARTS.RLEG)
+                {
+                    Matrix4x4 t1 = Transformations.Translate(0f, -0.4f, 0f);
+                    Matrix4x4 t2 = Transformations.Translate(0f, -0.5f, 0f);
+                    movementLegR = Mathf.Min(-rk.val * 5f,0);
+                    Matrix4x4 r = new Matrix4x4();
+                    if (rX.dir > 0) {
+                        if(0 <= currR) r = Transformations.RotateZ(-movementLegR);
+                        else r = Transformations.RotateZ(movementLegR);
+                    } 
+                    else r = Transformations.RotateZ(0);
+                    
+                    
+                    //r = Transformations.RotateZ(-rk.val*5f);
+                    m = accumT * t1 * r * t2 * m_scales[i];
+                    accumT *= t1 * r * t2;
+                }
+                else
+                {
+                    Matrix4x4 t1 = Transformations.Translate(0f, -0.5f, 0f);
+                    Matrix4x4 t2 = Transformations.Translate(-0.103f, -0.15f, 0f);
+                    movementLegR = Mathf.Max(rX.val * 4f, 0);
+                    Matrix4x4 r = Transformations.RotateZ(-rX.val * 2f);
+                    m = accumT * t1 * r * t2 * m_scales[i];
+                    accumT *= t1 * r * t2;
+                }
+
+                go_parts[i].GetComponent<MeshFilter>().mesh.vertices = Transformations.Transform(m, v3_originals);
+            }
+        }
     }
 }
